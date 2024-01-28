a67dd9c986748e9de480167ac2b68d27a402cd13
snplmntn <124994202+snplmntn@users.noreply.github.com>
2024-01-29 03:06:48 +0800

    Added Review Modal

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ import {
 
 let load = false;
 let haveUser;
+
+const apiUrl = 'https://fxt-api.dosshs.online/api';
+// const apiUrl = 'http://localhost:1234/api';
+
 //Checks User's Auth
 function checkAuthState() {
     auth.onAuthStateChanged(function (user) {
@@ -95,3 +99,58 @@ buyNow3.addEventListener("click", function () {
     window.location.href = "login.html";
   }
 });
+
+let modalBtn = document.getElementById("review-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+
+document.querySelector('#cancel-btn').addEventListener("click", async (e) => {
+  modal.style.display = "none"
+})
+
+document.querySelector('#submit-btn').addEventListener("click", async () => {
+  const feedback = document.querySelector('#feedback').value;
+
+  if(!feedback) return alert("Please input a feedback before submitting, thanks :)");
+  
+  const userID = localStorage.getItem("userID");
+
+  const review_data = JSON.stringify({
+    userID: userID,
+    feedback: feedback 
+ });
+
+ console.log(review_data)
+
+
+  try {
+    const response = await fetch(`${apiUrl}/review`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: review_data,
+    });
+
+    // Check if the response status is OK (200-299)
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+     }
+
+     alert("Thank you for the sweet or salty :( review. We appreciate your feedback and we'll keep it in mind :)!")
+  } catch (err) {
+    console.error(err)
+  }
+
+})
